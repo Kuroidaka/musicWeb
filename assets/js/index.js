677d5d7e797efdd8f3049b29f2a167ed6866d247
Canh <pcanhgm@gmail.com>
2022-08-18 01:04:22 +0700

    module song

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,10 @@
-$ = document.querySelector.bind(document);
-$$ = document.querySelectorAll.bind(document);
+import {songs} from './song.js'
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
 
-
+console.log (songs);
 
 let musicMenuHeight = $('.music-menu').clientHeight ; 
 
@@ -46,82 +48,15 @@ var playListWrapFix = $('.play-list-wrap');
 
         const PLAYER_STORAGE_KEY = 'WEB_PLAYER';
 
+
+
 const app = {
     currentIndex: 0,
     // isplaying : false,
     isRandom : false,
     isRepeat : false,
     config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},
-    songs: [
-        {
-            name: 'Perfect ',
-            singer: 'Ed Sheeran',
-            path: './assets/songs/Vietsub - Perfect - Ed Sheeran - Lyrics Video.mp3',
-            image: '/assets/img/song/Ed_Sheeran_Perfect_Music_Video-262499921-large.jpeg'
-        },
-        {
-            name: 'Yêu em hơn mỗi ngày',
-            singer: 'Andiez',
-            path: './assets/songs/Yêu Em Hơn Mỗi Ngày - Andiez - Official MV.mp3',
-            image: '/assets/img/song/Yêu em hơn mỗi ngày.jpg'
-        },
-        {
-            name: 'Buồn thì cứ khóc đi',
-            singer: 'Lynk Lee',
-            path: './assets/songs/Buồn Thì Cứ Khóc Đi - Lynk Lee - Official MV.mp3',
-            image: '/assets/img/song/buồn thì cứ khóc.jpeg'
-        },
-        {
-            name: 'Kẻ Theo Đuổi Ánh Sáng',
-            singer: 'Huy Vạc x Tiến Nguyễn',
-            path: './assets/songs/Kẻ Theo Đuổi Ánh Sáng - Huy Vạc x Tiến Nguyễn (Official MV).mp3',
-            image: '/assets/img/song/Kể theo đuổi ánh sáng.jpg'
-        },
-        {
-            name: 'Miền an nhiên',
-            singer: 'Phạm Minh Thành',
-            path: './assets/songs/Miền an nhiên -Lyrics- - Phạm Minh Thành.mp3',
-            image: '/assets/img/song/Miền an nhiên.jpeg'
-        },
-        {
-            name: 'Răng Khôn',
-            singer: 'PHÍ PHƯƠNG ANH',
-            path: './assets/songs/PHÍ PHƯƠNG ANH ft. RIN9 - Răng Khôn - Official Music Video.mp3',
-            image: '/assets/img/song/Răng khôn.jpeg'
-        },
-        {
-            name: 'Bông hoa đẹp nhất',
-            singer: 'QUÂN A.P',
-            path: './assets/songs/QUÂN A.P - BÔNG HOA ĐẸP NHẤT - OFFICIAL MUSIC VIDEO.mp3',
-            image: '/assets/img/song/Bông hoa đẹp nhất.jpeg'
-        },
-        {
-            name: 'Quên đặt tên',
-            singer: 'Phạm Nguyên Ngọc',
-            path: './assets/songs/QUÊN ĐẶT TÊN - Phạm Nguyên Ngọc (OFFICIAL MV).mp3',
-            image: '/assets/img/song/Quên đặt tên.jpeg'
-        },
-        {
-            name: 'Nghe Như Tình Yêu',
-            singer: 'HIEUTHUHAI',
-            path: './assets/songs/HIEUTHUHAI - Nghe Như Tình Yêu (prod. by Kewtiie) [Official Lyric Video].mp3',
-            image: '/assets/img/song/Nghe như tình yêu.jpeg'
-        },
-        {
-            name: 'LỜI ĐƯỜNG MẬT',
-            singer: 'HIEUTHUHAI',
-            path: './assets/songs/LỜI ĐƯỜNG MẬT - LYLY ft HIEUTHUHAI (Official Music Video).mp3',
-            image: '/assets/img/song/Lời đường mật.jpeg'
-        },
-        {
-            name: 'Xem Như Tôi Từng Cưới Được Cô Ấy',
-            singer: 'Mạc Khiếu Tỷ Tỷ',
-            path: './assets/songs/[Vietsub] Xem Như Tôi Từng Cưới Được Cô Ấy - Mạc Khiếu Tỷ Tỷ - 当我娶过她 - 莫叫姐姐.mp3',
-            image: '/assets/img/song/coi như tôi cưới cô ấy.jpeg'
-        },
-
-
-    ],
+    songs: songs,
     setConfig: function(key, value) {
         this.config[key] = value;
         localStorage.setItem(PLAYER_STORAGE_KEY,JSON.stringify(this.config));
@@ -211,7 +146,7 @@ const app = {
                 })
         }
 
-        pauseBtn.onclick = function() {
+        pauseBtn.onclick = () =>{
             audio.pause();
             this.animate([
                 {
@@ -253,7 +188,7 @@ const app = {
 
         // range move following the playing music
         
-        percentChange = function() {
+        var percentChange = function() {
             // console.log(range.value);
             if(audio.duration){
                 const currentPercent = audio.currentTime/audio.duration*100;  
@@ -334,14 +269,14 @@ const app = {
         //  click next button
 
         
-        activeCurrentSong =  function(activeIndex) {
+        var activeCurrentSong =  function(activeIndex) {
           
             currentSongItems[activeIndex].classList.remove('active');
             currentSongItems[_this.currentIndex].classList.add('active');
             
         }
 
-        changeView = function(currentActiveSongItem){
+        var changeView = function(currentActiveSongItem){
             setTimeout(() => {
                 currentActiveSongItem.scrollIntoView({
                     behavior: 'smooth',
@@ -393,7 +328,7 @@ const app = {
             currentSongItem.onclick = function() {
                 range.value = 0;
                 var activeIndex = _this.currentIndex;
-                currentActiveSongItem = $('.song-item.active');
+                var currentActiveSongItem = $('.song-item.active');
                 currentActiveSongItem.classList.remove('active');
                 currentSongItem.classList.add('active');
 
@@ -462,6 +397,8 @@ const app = {
                 return this.songs[this.currentIndex];
             }
         })
+
+        
     },
     playRandomSong : function() {
         
@@ -480,7 +417,8 @@ const app = {
         currentSongImage.style.backgroundImage = `url("${this.currentSong.image}")`;
         entireBackgroundImage.style.backgroundImage = `url("${this.currentSong.image}")`;
         entireBackgroundImageWrap.style.backgroundImage = `url("${this.currentSong.image}")`;
-        console.log(this.currentSong.image,currentSongImage);
+        // console.log(this.currentSong.image,currentSongImage);
+
         audio.src = this.currentSong.path;
     },
     loadConfig: function() {
@@ -594,7 +532,7 @@ const app = {
         this.checkCurrentSong();
         this.loadConfig();
         this.handleEvent();
-  
+       
 
     }   
 
