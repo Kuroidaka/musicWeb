017f6508ed4db0534266f095cda124b2eb882b69
Canh <pcanhgm@gmail.com>
2022-09-15 18:07:18 +0700

    fix animation play pause btn

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// import {songs} from './song.js'
+import {songs} from './song.js'
 // import playListView from './playListView.js';
 
 const $ = document.querySelector.bind(document);
@@ -64,76 +64,7 @@ const app = {
     isRepeat : false,
     isListOpen : false,
     config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},
-    songs: [
-        {
-            name: 'Perfect ',
-            singer: 'Ed Sheeran',
-            path: './assets/songs/Vietsub - Perfect - Ed Sheeran - Lyrics Video.mp3',
-            image: '/assets/img/song/Ed_Sheeran_Perfect_Music_Video-262499921-large.jpeg'
-        },
-        {
-            name: 'Yêu em hơn mỗi ngày',
-            singer: 'Andiez',
-            path: './assets/songs/Yêu Em Hơn Mỗi Ngày - Andiez - Official MV.mp3',
-            image: '/assets/img/song/Yêu em hơn mỗi ngày.jpg'
-        },
-        {
-            name: 'Buồn thì cứ khóc đi',
-            singer: 'Lynk Lee',
-            path: './assets/songs/Buồn Thì Cứ Khóc Đi - Lynk Lee - Official MV.mp3',
-            image: '/assets/img/song/buồn thì cứ khóc.jpeg'
-        },
-        {
-            name: 'Kẻ Theo Đuổi Ánh Sáng',
-            singer: 'Huy Vạc x Tiến Nguyễn',
-            path: './assets/songs/Kẻ Theo Đuổi Ánh Sáng - Huy Vạc x Tiến Nguyễn (Official MV).mp3',
-            image: '/assets/img/song/Kể theo đuổi ánh sáng.jpg'
-        },
-        {
-            name: 'Miền an nhiên',
-            singer: 'Phạm Minh Thành',
-            path: './assets/songs/Miền an nhiên -Lyrics- - Phạm Minh Thành.mp3',
-            image: '/assets/img/song/Miền an nhiên.jpeg'
-        },
-        {
-            name: 'Răng Khôn',
-            singer: 'PHÍ PHƯƠNG ANH',
-            path: './assets/songs/PHÍ PHƯƠNG ANH ft. RIN9 - Răng Khôn - Official Music Video.mp3',
-            image: '/assets/img/song/Răng khôn.jpeg'
-        },
-        {
-            name: 'Bông hoa đẹp nhất',
-            singer: 'QUÂN A.P',
-            path: './assets/songs/QUÂN A.P - BÔNG HOA ĐẸP NHẤT - OFFICIAL MUSIC VIDEO.mp3',
-            image: '/assets/img/song/Bông hoa đẹp nhất.jpeg'
-        },
-        {
-            name: 'Quên đặt tên',
-            singer: 'Phạm Nguyên Ngọc',
-            path: './assets/songs/QUÊN ĐẶT TÊN - Phạm Nguyên Ngọc (OFFICIAL MV).mp3',
-            image: '/assets/img/song/Quên đặt tên.jpeg'
-        },
-        {
-            name: 'Nghe Như Tình Yêu',
-            singer: 'HIEUTHUHAI',
-            path: './assets/songs/HIEUTHUHAI - Nghe Như Tình Yêu (prod. by Kewtiie) [Official Lyric Video].mp3',
-            image: '/assets/img/song/Nghe như tình yêu.jpeg'
-        },
-        {
-            name: 'LỜI ĐƯỜNG MẬT',
-            singer: 'HIEUTHUHAI',
-            path: './assets/songs/LỜI ĐƯỜNG MẬT - LYLY ft HIEUTHUHAI (Official Music Video).mp3',
-            image: '/assets/img/song/Lời đường mật.jpeg'
-        },
-        {
-            name: 'Xem Như Tôi Từng Cưới Được Cô Ấy',
-            singer: 'Mạc Khiếu Tỷ Tỷ',
-            path: './assets/songs/[Vietsub] Xem Như Tôi Từng Cưới Được Cô Ấy - Mạc Khiếu Tỷ Tỷ - 当我娶过她 - 莫叫姐姐.mp3',
-            image: '/assets/img/song/coi như tôi cưới cô ấy.jpeg'
-        },
-    
-    
-    ],
+    songs: [...songs],
     setConfig: function(key, value) {
         this.config[key] = value;
         localStorage.setItem(PLAYER_STORAGE_KEY,JSON.stringify(this.config));
@@ -204,67 +135,89 @@ const app = {
         // check playing
 
         const playBtnActive = $('.play-icon.play_active');
-        playBtn.onclick = function() { 
-                audio.play();    
-                _this.isPlaying = true;
-                _this.changeSizeImage();
-                pauseBtn.animate([
-                    {
-                        transform: 'scale(1)'   
-                    },
-                    {
-                        transform: 'scale(0.8)'   
-                    },
-                    {
-                        transform: 'scale(1)'   
-                    }
 
-                ],
-                {
-                    duration: 500,
-                    iterations: 1
-                })
+        const handleBtnClick = (e) => {
+         
+
         }
-
-        pauseBtn.onclick = function() {
-            audio.pause();
-            _this.isPlaying = false;
-            _this.changeSizeImage();
-            playBtn.animate([
+        
+        playBtn.onclick = function() { 
+            pauseBtn.animate([
+                
                 {
-                    transform: 'scale(1)'   
+                    transform: 'scale(0)'   
                 },
                 {
-                    transform: 'scale(0.8)'   
-                },
-                {
-                    transform: 'scale(1)'   
+                    transform: 'scale(1.3)'   
                 }
 
             ],
             {
-                duration: 500,
+                duration: 200,
                 iterations: 1
             })
+
+            audio.play();   
+            _this.isPlaying = true;
+            _this.changeSizeImage();
+
+            audio.onplay = function () {
+                playBtn.style.display = 'none';
+                pauseBtn.style.display = 'block';
+                currentSongImageRotate.play();
+    
+                // currentSongImage.classList.remove('active');
+                
+                // console.log(currentSongImageRotate.style.transform);
+               
+            }
+      
+                
+        }
+
+       
+
+        pauseBtn.onclick = function() { 
+            playBtn.animate([
+                
+                {
+                    transform: 'scale(0)'   
+                },
+                {
+                    transform: 'scale(1.3)'   
+                }
+
+            ],
+            {
+                duration: 200,
+                iterations: 1
+            })
+
+            audio.pause();   
+            _this.isPlaying = false;
+            _this.changeSizeImage();
+
+            audio.onpause = function () {
+                pauseBtn.style.display = 'none';
+                playBtn.style.display = 'block';
+                currentSongImageRotate.pause();
+    
+                // currentSongImage.classList.remove('active');
+                
+                // console.log(currentSongImageRotate.style.transform);
+               
+            }
+      
+                
         }
 
         this.menuIconchange();
         //  change playing pausing icon
 
-        audio.onplay = function() {
-            playBtn.style.display = 'none';
-            pauseBtn.style.display = 'block';
-            currentSongImageRotate.play();
-
-            // currentSongImage.classList.remove('active');
-            
-            // console.log(currentSongImageRotate.style.transform);
-           
-        }
+       
 
         audio.onpause = function() {
-            playBtn.style.display = 'block';
-            pauseBtn.style.display = 'none';
+           
             currentSongImageRotate.pause();
         }
 
